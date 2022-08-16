fbb31b8e4907addde1cb9987942c8a9b7e528136
Hillary Muller <hillaryaward@gmail.com>
2022-08-16 17:25:10 -0500

    commit tree 1f7bfed7a67193fce42937a4f135f462f4ab62cb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind
+    if one + two <= three || two + three <= one || one + three <= two || (one * two * three) == 0
+      begin 
+        raise TriangleError
+        puts error.message
+      end
+
+    elsif one == two && two == three
+      :equilateral
+    elsif one == two || one == three || two == three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+    class TriangleError < StandardError
+      def message
+        "this is not a valid triangle"
+      end
+    end
+
 end
